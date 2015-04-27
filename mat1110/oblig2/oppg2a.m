a2cad6dd65c5d9a76c62e60e40c3f76d3d2aadf0
Erik Gåserud <erik.gaaserud@gmail.com>
2015-04-28 01:34:28 +0200

    Ferdigstilt oblig2

--- mat1110/oblig2/oppg2a.m
+++ mat1110/oblig2/oppg2a.m
@@ -1,5 +1,4 @@
 function [ output ] = f( a, x, N )
-% a går fra 0 til pi, x er en startverdi fra 0 til pi, N er antall iterasjoner.
 output(1)=a.*sin(x);
 for i = 2:N
         output(i) = a.*sin(output(i-1));
